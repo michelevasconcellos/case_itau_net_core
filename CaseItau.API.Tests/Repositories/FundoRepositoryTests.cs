ca361fcdcaece0af2eea473fce25bb8ea315a624
Michele Vasconcellos <q.michele@hotmail.com>
2026-04-27 13:38:50 -0300

    Criação do projeto de testes unitários com xUnit

---
+++ CaseItau.API.Tests/Repositories/FundoRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using CaseItau.API.Repositories;
+using CaseItau.API.Domain.Entities;
+
+namespace CaseItau.API.Tests.Repositories
+{
+    public class FundoRepositoryTests
+    {
+        private readonly FundoRepository _repository;
+
+        public FundoRepositoryTests()
+        {
+            SQLitePCL.Batteries.Init();
+            _repository = new FundoRepository();
+        }
+
+        [Fact]
+        public void Add_DeveCadastrarNovoFundo()
+        {
+            var fundo = new Fundo
+            {
+                Codigo = "TESTE001",
+                Nome = "Fundo Teste",
+                Cnpj = "99999999999999",
+                CodigoTipo = 1,
+                Patrimonio = 1000
+            };
+
+            if (_repository.ExistsByCodigo(fundo.Codigo))
+            {
+                _repository.Delete(fundo.Codigo);
+            }
+
+            _repository.Add(fundo);
+
+            var resultado = _repository.GetByCodigo(fundo.Codigo);
+
+            Assert.NotNull(resultado);
+            Assert.Equal("TESTE001", resultado.Codigo);
+        }
+
+        [Fact]
+        public void GetAll_DeveRetornarListaDeFundos()
+        {
+            // Act
+            var resultado = _repository.GetAll();
+
+            // Assert
+            Assert.NotNull(resultado);
+            Assert.NotEmpty(resultado);
+        }
+
+    }
+}
